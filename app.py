630a23be84b609579b34006a1fafefeb6aef9b56
Imran Nazir <immudon17@gmail.com>
2025-05-21 20:35:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 def is_even(n):
+    """Return True if n is even, False otherwise."""
     return n % 2 == 0
 
 def is_odd(n):
+    """Return True if n is odd, False otherwise."""
     return n % 2 != 0
 
 if __name__ == "__main__":
-    num = int 8
+    num = int(input("Enter a number: 8"))
     print(f"{num} is even: {is_even(num)}")
     print(f"{num} is odd: {is_odd(num)}")
